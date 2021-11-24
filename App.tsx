1e3d97acc7799448b9cdda835cb7422843b677d4
ygor.verissimo <ygor.verissimo@netshow.me>
2021-11-24 10:33:14 -0300

    Add gameplay repository

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import { Background } from './src/components/Background'
+import AppLoading from 'expo-app-loading'
+import { StatusBar, LogBox } from 'react-native'
+import { useFonts } from 'expo-font';
+import { Routes } from './src/routes'
+import { AuthProvider } from './src/hooks/auth'
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.'])
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Background>
+  );
+}
+
